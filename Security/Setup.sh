b2f63f41774a4b50ebcf17453c5142e08b571210
Cyber Alamo <107036163+CyberAlamo@users.noreply.github.com>
2022-06-18 17:00:34 -0400

    Update Setup.sh

--- Security/Setup.sh
+++ Security/Setup.sh
@@ -2,9 +2,37 @@
 //security tool installer
 //6/18/2022
 
-//updates the system & installs ansible
+//updates the system
 sudo swupd check-update
 sudo swupd update
+
+//downloads software according to user specifications
+
 sudo swupd bundle-add ansible
-
-
+sudo swupd bundle-add suricata
+sudo swupd bundle-add audit
+sudo swupd bundle-add boot-encrypted
+sudo swupd bundle-add bootloader-extras
+sudo swupd bundle-add clamav
+sudo swupd bundle-add cryptography
+sudo swupd bundle-add cryptoprocessor-management
+sudo swupd bundle-add cve-bin-tool
+sudo swupd bundle-add dovecot
+sudo swupd bundle-add firewalld
+sudo swupd bundle-add gpgme
+sudo swupd bundle-add keepalived
+sudo swupd bundle-add keepassxc
+sudo swupd bundle-add kleopatra
+sudo swupd bundle-add letsencrypt-client
+sudo swupd bundle-add lib-openssl
+sudo swupd bundle-add ncat
+sudo swupd bundle-add network-security-monitoring
+sudo swupd bundle-add nmap
+sudo swupd bundle-add p11-kit
+sudo swupd bundle-add pass
+sudo swupd bundle-add pdns
+sudo swupd bundle-add polkit
+sudo swupd bundle-add pwgen
+sudo swupd bundle-add socat
+sudo swupd bundle-add suricata
+sudo swupd bundle-add wpa_supplicant
